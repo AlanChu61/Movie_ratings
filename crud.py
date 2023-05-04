1d20dff11522a23ff2185c09c11f4de9bae09182
Alan Hwader Chu <alanhdchu@AlanM1.local>
2023-05-04 09:54:24 -0700

    added view detail for movie and user

--- crud.py
+++ crud.py
@@ -4,6 +4,7 @@ from model import db, User, Movie, Rating, connect_to_db
 
 # Functions start here!
 
+
 def create_user(email, password):
     """Create and return a new user."""
 
@@ -11,16 +12,36 @@ def create_user(email, password):
 
     return user
 
+
+def get_users():
+    return User.query.all()
+
+
+def get_user_by_id(id):
+    return User.query.get(id)
+
+
 def create_movie(title, overview, release_date, poster_path):
     """Create and return a new movie."""
-    
-    movie = Movie(title=title, overview=overview, release_date=release_date, poster_path=poster_path)
+
+    movie = Movie(title=title, overview=overview,
+                  release_date=release_date, poster_path=poster_path)
 
     return movie
 
+
+def get_movies():
+    return Movie.query.all()
+
+
+def get_movie_by_id(id):
+    return Movie.query.get(id)
+
+
 def create_rating(user, movie, score):
-    rating = Rating(user=user,score=score,movie=movie)
+    rating = Rating(user=user, score=score, movie=movie)
     return rating
+
 
 if __name__ == '__main__':
     from server import app
--- seed_database.py
+++ seed_database.py
@@ -9,15 +9,15 @@ import crud
 import model
 import server
 
-#1 drop db and create db
+# 1 drop db and create db
 os.system('dropdb ratings')
 os.system('createdb ratings')
 
-#2 connect to db and create all tables from db
+# 2 connect to db and create all tables from db
 model.connect_to_db(server.app)
 model.db.create_all()
 
-#3 load data
+# 3 load data
 # Load movie data from JSON file
 with open('data/movies.json') as f:
     movie_data = json.loads(f.read())
@@ -31,29 +31,29 @@ for movie in movie_data:
     # datetime object with datetime.strptime
     title = movie["title"]
     overview = movie["overview"]
-    poster_path =  movie["poster_path"]
+    poster_path = movie["poster_path"]
     # "2020-05-15"
     format = "%Y-%m-%d"
-    release_date = datetime.strptime(movie['release_date'],format)
+    release_date = datetime.strptime(movie['release_date'], format)
     # TODO: create a movie here and append it to movies_in_db
     new_movie = crud.create_movie(title, overview, release_date, poster_path)
     movies_in_db.append(new_movie)
 model.db.session.add_all(movies_in_db)
 model.db.session.commit()
 
-#4 create user and randomly choose moive and rated it with 1-5 randomly
+# 4 create user and randomly choose moive and rated it with 1-5 randomly
 for n in range(10):
     email = f'user{n}@test.com'  # Voila! A unique email!
     password = 'test'
     # TODO: create a user here
-    user = crud.create_user(email,password)
+    user = crud.create_user(email, password)
     model.db.session.add(user)
     # TODO: create 10 ratings for the user
-    for i in range(0,11,1):
+    for i in range(0, 11, 1):
         random_movie = choice(movies_in_db)
-        random_score = randint(1,5)
+        random_score = randint(1, 5)
         #create_rating(user, movie, score)
-        rating = crud.create_rating(user,random_movie,random_score)
+        rating = crud.create_rating(user, random_movie, random_score)
         model.db.session.add(rating)
-        # model.db.session.commit() 
-model.db.session.commit()  
+        # model.db.session.commit()
+model.db.session.commit()
